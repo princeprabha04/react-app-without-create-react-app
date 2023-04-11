d15a1804ed8831a69e655093636e4b227a8a5f59
Lavakumar Thandalamm <lavakumar.t2011@gmail.com>
2023-04-11 22:28:54 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import MySVGIcon from "./MySVGIcon";
 
 function App() {
+  console.log("prince-prabha")
   return (
     <div>
       <MySVGIcon iconName="Delete-Icon" />
